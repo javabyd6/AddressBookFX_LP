b13a61f543592d6797bbfbe0123a89c5052c401d
Lukasz Polinski <lukasz.polinski@gmail.com>
2019-03-23 10:01:09 +0100

    root

---
+++ src/main/java/pl/sda/AddressBookFX_LP/Main.java
@@ -0,0 +1,24 @@
+package pl.sda.AddressBookFX_LP;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root =
+                FXMLLoader.load(
+                        getClass()
+                                .getResource("/root.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
+
